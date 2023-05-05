3cad336c7991548b0ac22899b84fa6c6de7f15ef
Kalil Georges Balech <kalilgb152@gmail.com>
2023-05-05 10:22:42 -0300

    nodemon and basics npm run scripts created

--- 10-JobbeeAPI/app.js
+++ 10-JobbeeAPI/app.js
@@ -7,7 +7,8 @@ app.use(express.json())
 dotenv.config({path: './config/config.env'})
 
 const PORT = process.env.PORT
+const ENVIRONMENT = process.env.NODE_ENV
 
 app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port ${PORT} in ${ENVIRONMENT} mode`)
 })
